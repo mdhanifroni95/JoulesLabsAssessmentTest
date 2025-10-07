31c6a1c66c0e9702b13b9d9f191a27f6487b1b9c
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2025-10-07 19:34:13 +0600

    feat: enable global validation in main file with ValidationPipe configuration

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,14 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //global validation in main file
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //   }),
-  // );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
   // console.log(process.env.url);
-
 }
 bootstrap();
